98655ea2a61fec7a82a690103e230d71cfd6eb9f
NCrashed <NCrashed@gmail.com>
2012-08-21 02:28:54 +0400

    Added some test code to check GtkD.lib. It was very hard to compile gtkd, code patches, dmd2 2.060 fixes... Soon will write some tutorials to start develop Hades.

---
+++ src/main.d
@@ -0,0 +1,54 @@
+module main;
+
+import std.stdio;
+
+import gtk.MainWindow;
+import gtk.AboutDialog;
+import gtk.Label;
+import gtk.Button;
+import gtk.VBox;
+import gtk.Main;
+
+class ButtonUsage : MainWindow
+{
+	Label StatusLbl;
+	this()
+	{
+		super("Button Usage");
+		setDefaultSize(200, 100);
+		VBox box = new VBox(false, 10);
+		StatusLbl = new Label("Click a Button");
+		box.add(StatusLbl);
+		box.add(new Button("Button 1", &onBtn1));
+		box.add(new Button("Exit", &onBtn2));
+		box.add(new Button("About", &onBtn3));
+		add(box);
+		showAll();	
+	}
+	void onBtn1(Button button)
+	{
+		StatusLbl.setText("You Clicked Button 1");
+	}
+	void onBtn2(Button button)
+	{
+		Main.exit(0);
+	}
+	void onBtn3(Button button)
+	{
+		with (new AboutDialog())
+		{
+			string[] names;
+			names ~= "This is test";
+			setAuthors(names);
+			setWebsite("https://github.com/NCrashed/HadesIDE");
+			showAll();
+		}
+	}
+}
+
+void main(string[] args)
+{
+	Main.init(args);
+	new ButtonUsage();
+	Main.run();
+}
